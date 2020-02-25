19b2835b311d7bcc70c21b334220880fc5625ed9
xrudoga <defcw58@gmail.com>
2020-02-25 17:56:48 +0800

    Private and Admin Route With User Dashboard

--- src/auth/index.js
+++ src/auth/index.js
@@ -43,3 +43,31 @@ export const authenticate = (data, next) => {
     }
 }
 
+export const signout = (next) => {
+    if(typeof window !== 'undefined') {
+        localStorage.removeItem('jwt')
+        next();
+        return fetch(`${API}/signout`, {
+            method: 'GET'
+        })
+            .then(response => {
+                console.log('signout', response)
+            })
+            .catch(err => console.log(err))
+    }
+}
+
+export const isAuthenticated = () => {
+    if (typeof window == 'undefined') {
+        return false
+    }
+    if (localStorage.getItem('jwt') === '{"err":"User with that email does not exist. Please signup."}'){
+        return false
+    }
+
+    if (localStorage.getItem('jwt')) {
+        return JSON.parse(localStorage.getItem('jwt'))
+    } else {
+        return false
+    }
+}
