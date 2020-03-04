c2c7fab3bea7622df05c54457b5899b1a2aaf751
xrudoga <defcw58@gmail.com>
2020-03-04 12:40:22 +0800

    User purchase history

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../core/Layout'
 import { isAuthenticated } from '../auth'
 import { Link } from 'react-router-dom'
+import { getPurchaseHistory } from './apiUser'
+import moment from 'moment'
 
 const Dashboard = () => {
+    const [history, setHistory] = useState([])
 
-    const {user: { name, email, role}} = isAuthenticated()
+    const {
+        user: { _id, name, email, role}
+    } = isAuthenticated()
+
+    const token =isAuthenticated().token
+
+    const init = (userId, token) => {
+        getPurchaseHistory(userId, token).then(data => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                setHistory(data)
+            }
+        })
+    }
+
+    useEffect(() => {
+        init(_id, token)
+    }, [])
 
     const userLinks = () => {
         return (
@@ -16,7 +37,7 @@ const Dashboard = () => {
                         <Link className='nav-link' to='/cart'>My Cart</Link>
                     </li>
                     <li className='list-group-item'>
-                        <Link className='nav-link' to='/profile/update'>Upade Profile</Link>
+                        <Link className='nav-link' to={`/profile/${_id}`}>Upade Profile</Link>
                     </li>
                 </ul>
             </div>
@@ -38,12 +59,32 @@ const Dashboard = () => {
         )
     }
 
-    const purchaseHistory = () => {
+    const purchaseHistory = (history) => {
         return (
             <div className='card mb-5'>
                 <h3 className='card-header'>Purchase history</h3>
                 <ul className='list-group'>
-                    <li className='list-group-item'>history</li>
+                    <li className='list-group-item'>
+                        {history.map((h, i) => {
+                            return (
+                                <div key={i}>
+                                    <hr />
+                                    {h.products.map((p, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <h6>Product name: {p.name}</h6>
+                                                <h6>Product price: ${p.price}</h6>
+                                            </div>
+                                        );
+                                    })}
+                                    <h6>
+                                        Purchased date:{" "}
+                                        {moment(h.createdAt).fromNow()}
+                                    </h6>
+                                </div>
+                            );
+                        })}
+                    </li>
                 </ul>
             </div>
         )
@@ -61,7 +102,7 @@ const Dashboard = () => {
                 </div>
                 <div className='col-9'>
                     {userInfo()}
-                    {purchaseHistory()}
+                    {purchaseHistory(history)}
                 </div>
             </div>
         </Layout>
